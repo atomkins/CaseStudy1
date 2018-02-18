ffd2f13e0c2533f3c2bc979fecb830c150bb4a46
drcrod <carollr@smu.edu>
2018-02-18 15:33:31 -0600

    Add Presentation Outline to GitHub

--- Makefile.R
+++ Makefile.R
@@ -1,4 +1,4 @@
-knitr::knit("casestudy1_MSDS6306_sec402.Rmd")
+knitr::knit("./casestudy1_MSDS6306_sec402.Rmd")
 markdown::markdownToHTML("casestudy1_MSDS6306_sec402.md",output="casestudy1_MSDS6306_sec402.html")
 knitr::knit("Codebook.Rmd")
 markdown::markdownToHTML("Codebook.md",output="Codebook.html")
